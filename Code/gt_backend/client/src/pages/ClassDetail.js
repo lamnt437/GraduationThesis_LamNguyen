edff6a99352b4be9371f70ca973971c6896a0e4b
Lam Nguyen <lamnt437@gmail.com>
2021-05-01 09:39:30 +0700

    add class Request ui basic flow

--- Code/gt_backend/client/src/pages/ClassDetail.js
+++ Code/gt_backend/client/src/pages/ClassDetail.js
@@ -8,22 +8,29 @@ import Header from '../components/classroom/header/Header';
 import Sidebar from '../components/classroom/sidebar/Sidebar';
 import Feed from '../components/classroom/tabs/feed/Feed';
 import Widget from '../components/classroom/widget/Widget';
+import ClassRequest from '../components/classroom/ClassRequest';
 
 const ClassDetail = (props) => {
   const params = useParams();
 
   const [classDetail, setClassDetail] = useState({});
+  const [isRelated, setIsRelated] = useState(false);
+  const [isFound, setIsFound] = useState(false);
 
   useEffect(async () => {
     // TODO validate res isRelated here
     try {
       const res = await fetchClassroom(params.id);
       console.log(res);
+      // setClassDetail(res.data.classroom);
+      setIsRelated(true);
+      setIsFound(true);
     } catch (error) {
       console.error(error.message);
       console.log(error.response);
       if (error.response.status === 401) {
         // redirect to ClassRequest
+        setIsFound(true);
         console.log('redirect to classrequest');
       }
     }
@@ -32,15 +39,16 @@ const ClassDetail = (props) => {
     // fetch a class
     // set state
   }, []);
-  return (
-    <Fragment>
-      {/* <ClassDetailComponent
-        name={classDetail.name}
-        description={classDetail.description}
-        classId={classDetail._id}
-      /> */}
-    </Fragment>
-  );
+
+  let renderedComp = <div>Not Found</div>;
+  if (isFound) {
+    if (isRelated) {
+      renderedComp = <div>Ok</div>;
+    } else {
+      renderedComp = <ClassRequest classId={params.id} />;
+    }
+  }
+  return renderedComp;
 };
 
 export default ClassDetail;
